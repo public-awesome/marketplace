5ccc649d59729ea4d11050173abf4c35cfa03510
Shane Vitarana <shanev@gmail.com>
2022-03-26 18:17:23 -0400

    Removed unused code

--- contracts/marketplace/src/state.rs
+++ contracts/marketplace/src/state.rs
@@ -1,4 +1,3 @@
-use crate::error::ContractError;
 use cosmwasm_std::{Addr, Coin};
 use cw_storage_plus::Map;
 use schemars::JsonSchema;
@@ -10,20 +9,6 @@ pub struct Bid {
     pub bidder: Addr,
 }
 
-// impl Bid {
-//     /// Checks if a bid is valid
-//     pub fn is_valid(&self) -> Result<bool, ContractError> {
-//         let bid_amount = &self.amount;
-
-//         // Check amount is not zero
-//         if bid_amount.amount.is_zero() {
-//             return Err(ContractError::InvalidBidTooLow {});
-//         }
-
-//         Ok(true)
-//     }
-// }
-
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Ask {
     pub amount: Coin,
@@ -34,46 +19,3 @@ pub const TOKEN_BIDS: Map<(&Addr, &str, &Addr), Bid> = Map::new("token_bidders")
 
 // Mapping from (collection, token_id) to the current ask for the token
 pub const TOKEN_ASKS: Map<(&Addr, &str), Ask> = Map::new("token_asks");
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use cosmwasm_std::{coin, Addr};
-//     use sg_std::NATIVE_DENOM;
-
-//     #[test]
-//     fn valid_bids() {
-//         let bidder: Addr = Addr::unchecked("bidder");
-//         let creator: Addr = Addr::unchecked("creator");
-
-//         // Normal bid
-//         let bid = Bid {
-//             amount: coin(100, NATIVE_DENOM),
-//             bidder: bidder.clone(),
-//             recipient: creator.clone(),
-//         };
-//         assert!(bid.is_valid().is_ok());
-
-//         // High number
-//         let bid = Bid {
-//             amount: coin(1000000000000, NATIVE_DENOM),
-//             bidder,
-//             recipient: creator,
-//         };
-//         assert!(bid.is_valid().is_ok());
-//     }
-
-//     #[test]
-//     fn invalid_bids() {
-//         let bidder: Addr = Addr::unchecked("bidder");
-//         let creator: Addr = Addr::unchecked("creator");
-
-//         // Low number
-//         let bid = Bid {
-//             amount: coin(0, NATIVE_DENOM),
-//             bidder,
-//             recipient: creator,
-//         };
-//         assert!(bid.is_valid().is_err());
-//     }
-// }
