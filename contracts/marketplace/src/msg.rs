2803545ef04e18a40e01af1b39c20a8fed9945cc
Shane Vitarana <shanev@gmail.com>
2022-04-10 18:31:22 -0400

    Removed TODO

--- contracts/marketplace/src/msg.rs
+++ contracts/marketplace/src/msg.rs
@@ -37,7 +37,6 @@ pub enum ExecuteMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // TODO: add ask queries by seller
     /// Get the current ask for specific NFT
     /// Return type: `CurrentAskResponse`
     CurrentAsk {
