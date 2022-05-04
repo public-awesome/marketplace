78012c259af143523895a3f07f3babf140d30b1d
Shane Vitarana <shanev@gmail.com>
2022-05-04 09:11:17 -0400

    Add min bid amount

--- types/contracts/marketplace/sudo_msg.d.ts
+++ types/contracts/marketplace/sudo_msg.d.ts
@@ -1,33 +1,38 @@
 import { ExpiryRange, Uint128 } from "./shared-types";
 
-export type SudoMsg = ({
-update_params: {
-ask_expiry?: (ExpiryRange | null)
-bid_expiry?: (ExpiryRange | null)
-max_finders_fee_bps?: (number | null)
-min_price?: (Uint128 | null)
-operators?: (string[] | null)
-trading_fee_bps?: (number | null)
-[k: string]: unknown
-}
-} | {
-add_ask_created_hook: {
-hook: string
-[k: string]: unknown
-}
-} | {
-remove_ask_created_hook: {
-hook: string
-[k: string]: unknown
-}
-} | {
-add_ask_filled_hook: {
-hook: string
-[k: string]: unknown
-}
-} | {
-remove_ask_filled_hook: {
-hook: string
-[k: string]: unknown
-}
-})
+export type SudoMsg =
+  | {
+      update_params: {
+        ask_expiry?: ExpiryRange | null;
+        bid_expiry?: ExpiryRange | null;
+        max_finders_fee_bps?: number | null;
+        min_price?: Uint128 | null;
+        operators?: string[] | null;
+        trading_fee_bps?: number | null;
+        [k: string]: unknown;
+      };
+    }
+  | {
+      add_ask_created_hook: {
+        hook: string;
+        [k: string]: unknown;
+      };
+    }
+  | {
+      remove_ask_created_hook: {
+        hook: string;
+        [k: string]: unknown;
+      };
+    }
+  | {
+      add_ask_filled_hook: {
+        hook: string;
+        [k: string]: unknown;
+      };
+    }
+  | {
+      remove_ask_filled_hook: {
+        hook: string;
+        [k: string]: unknown;
+      };
+    };
