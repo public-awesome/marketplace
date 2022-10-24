fb45603d6b75603700f587a829dafd37000650ee
Shane Vitarana <shanev@gmail.com>
2022-10-23 23:47:44 -0400

    Add Auction type

--- contracts/reserve-auction/src/state.rs
+++ contracts/reserve-auction/src/state.rs
@@ -1,8 +1,9 @@
+use cw_storage_macro::index_list;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, MultiIndex};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 pub struct State {
@@ -11,3 +12,28 @@ pub struct State {
 }
 
 pub const STATE: Item<State> = Item::new("state");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct Auction {
+    pub seller: Addr,
+    pub reserve_price: u128,
+    pub payment_address: Addr,
+    pub highest_bid: u64,
+    pub highest_bidder: Addr,
+    pub duration: u64,
+    pub start_time: u64,
+    pub first_bid_time: u64,
+}
+
+pub type TokenId = String;
+
+pub type AuctionKey = (Addr, TokenId);
+
+#[index_list(Auction)]
+pub struct AuctionIndexes<'a> {
+    pub seller: MultiIndex<'a, Addr, Auction, AuctionKey>,
+    pub highest_bid: MultiIndex<'a, u64, Auction, AuctionKey>,
+    pub duration: MultiIndex<'a, u64, Auction, AuctionKey>,
+    pub start_time: MultiIndex<'a, u64, Auction, AuctionKey>,
+    pub first_bid_time: MultiIndex<'a, u64, Auction, AuctionKey>,
+}
