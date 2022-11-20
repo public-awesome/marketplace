5cdb5787b62111f97c33014d8a86f423c15a62bb
Humanalgorithm <scottomj@gmail.com>
2022-11-20 18:01:54 -0500

    change constants on unit tests

--- contracts/marketplace/src/testing/setup/constants.rs
+++ contracts/marketplace/src/testing/setup/constants.rs
@@ -24,3 +24,5 @@ pub const MIN_EXPIRY: u64 = 24 * 60 * 60; // 24 hours (in seconds)
 pub const MAX_EXPIRY: u64 = 180 * 24 * 60 * 60; // 6 months (in seconds)
 pub const MAX_FINDERS_FEE_BPS: u64 = 1000; // 10%
 pub const BID_REMOVAL_REWARD_BPS: u64 = 500; // 5%
+
+pub const TRADING_FEE_BASIS_POINTS: u64 = 200; // 2%
