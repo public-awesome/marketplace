f735d45150c4f5c5217c64586e8067872540f1a7
John Y <j@publicawesome.com>
2022-12-01 11:04:34 -0500

    use sg2 mock params

--- contracts/marketplace/src/testing/setup/mock_collection_params.rs
+++ contracts/marketplace/src/testing/setup/mock_collection_params.rs
@@ -2,87 +2,52 @@ use cosmwasm_std::{Decimal, Timestamp};
 use sg721::{CollectionInfo, RoyaltyInfoResponse};
 
 use sg2::msg::CollectionParams;
+use sg2::tests::mock_collection_params;
 
 pub fn mock_collection_params_1(start_trading_time: Option<Timestamp>) -> CollectionParams {
     CollectionParams {
-        code_id: 1,
-        name: "Collection Name".to_string(),
-        symbol: "COL".to_string(),
         info: CollectionInfo {
-            creator: "creator".to_string(),
-            description: String::from("Stargaze Monkeys"),
-            image: "https://example.com/image.png".to_string(),
-            external_link: Some("https://example.com/external.html".to_string()),
             start_trading_time,
-            explicit_content: Some(false),
-            royalty_info: Some(RoyaltyInfoResponse {
-                payment_address: "creator".to_string(),
-                share: Decimal::percent(10),
-            }),
+            ..mock_collection_params().info
         },
+        ..mock_collection_params()
     }
 }
 
 pub fn mock_curator_payment_address(start_trading_time: Option<Timestamp>) -> CollectionParams {
     CollectionParams {
-        code_id: 1,
-        name: String::from("Test Coin"),
-        symbol: String::from("TEST"),
         info: CollectionInfo {
-            creator: "creator".to_string(),
-            description: String::from("Stargaze Monkeys"),
-            image: "https://example.com/image.png".to_string(),
-            external_link: Some("https://example.com/external.html".to_string()),
             royalty_info: Some(RoyaltyInfoResponse {
                 payment_address: "curator".to_string(),
                 share: Decimal::percent(10),
             }),
             start_trading_time,
-            explicit_content: None,
+            ..mock_collection_params().info
         },
+        ..mock_collection_params()
     }
 }
 
 pub fn mock_collection_params_high_fee(start_trading_time: Option<Timestamp>) -> CollectionParams {
     CollectionParams {
-        code_id: 1,
-        name: String::from("Test Coin"),
-        symbol: String::from("TEST"),
         info: CollectionInfo {
-            creator: "creator".to_string(),
-            description: String::from("Stargaze Monkeys"),
-            image:
-                "ipfs://bafybeigi3bwpvyvsmnbj46ra4hyffcxdeaj6ntfk5jpic5mx27x6ih2qvq/images/1.png"
-                    .to_string(),
-            external_link: Some("https://example.com/external.html".to_string()),
             royalty_info: Some(RoyaltyInfoResponse {
                 payment_address: "creator".to_string(),
                 share: Decimal::percent(100),
             }),
             start_trading_time,
-            explicit_content: None,
+            ..mock_collection_params().info
         },
+        ..mock_collection_params()
     }
 }
 
 pub fn mock_collection_two(start_trading_time: Option<Timestamp>) -> CollectionParams {
     CollectionParams {
-        code_id: 1,
-        name: String::from("Test Collection 2"),
-        symbol: String::from("TEST 2"),
         info: CollectionInfo {
-            creator: "creator".to_string(),
-            description: String::from("Stargaze Monkeys 2"),
-            image:
-                "ipfs://bafybeigi3bwpvyvsmnbj46ra4hyffcxdeaj6ntfk5jpic5mx27x6ih2qvq/images/1.png"
-                    .to_string(),
-            external_link: Some("https://example.com/external.html".to_string()),
-            royalty_info: Some(RoyaltyInfoResponse {
-                payment_address: "creator".to_string(),
-                share: Decimal::percent(10),
-            }),
             start_trading_time,
-            explicit_content: None,
+            ..mock_collection_params().info
         },
+        ..mock_collection_params()
     }
 }
