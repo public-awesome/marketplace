9786eb9f330b0396b47d7440449e481f4c835ad4
Tasio Victoria <tasiovictoria@gmail.com>
2023-04-25 15:26:08 -0400

    Add load_marketplace_params and load_collection_royalties functions to sg-marketplace-common

--- packages/sg-marketplace-common/src/tests/unit_tests.rs
+++ packages/sg-marketplace-common/src/tests/unit_tests.rs
@@ -1,15 +1,21 @@
 use crate::{
-    bank_send, calculate_nft_sale_fees, has_approval, only_owner, owner_of, payout_nft_sale_fees,
-    transfer_nft, TokenPayment, TransactionFees,
+    bank_send, calculate_nft_sale_fees, has_approval, load_collection_royalties,
+    load_marketplace_params, only_owner, owner_of, payout_nft_sale_fees, transfer_nft,
+    TokenPayment, TransactionFees,
 };
 
 use cosmwasm_std::{
-    coin, testing::mock_info, to_binary, Addr, BankMsg, ContractResult, Decimal, Querier,
-    QuerierResult, QuerierWrapper, StdError, SystemResult, Uint128,
+    coin,
+    testing::{mock_dependencies, mock_info},
+    to_binary, Addr, BankMsg, ContractResult, Decimal, Querier, QuerierResult, QuerierWrapper,
+    StdError, SystemResult, Uint128,
 };
 use cw721::OwnerOfResponse;
+use cw_utils::Duration;
 use mockall::*;
-use sg721::RoyaltyInfo;
+use sg721::{RoyaltyInfo, RoyaltyInfoResponse};
+use sg721_base::msg::CollectionInfoResponse;
+use sg_marketplace::{msg::ParamsResponse, state::SudoParams, ExpiryRange};
 use sg_std::{CosmosMsg, Response, StargazeMsg, StargazeMsgWrapper, NATIVE_DENOM};
 use test_suite::common_setup::templates::base_minter_with_sg721;
 
@@ -149,6 +155,104 @@ fn try_has_approval() {
             &token_id.to_string(),
             None
         )
+    );
+}
+
+#[test]
+fn try_load_marketplace_params() {
+    mock! {
+        QuerierStruct {}
+        impl Querier for QuerierStruct {
+            fn raw_query(&self, bin_request: &[u8]) -> QuerierResult;
+        }
+    }
+
+    let listing_fee: u128 = 10;
+    let trading_fee_bps = 200;
+    let min_expiry: u64 = 24 * 60 * 60; // 24 hours (in seconds)
+    let max_expiry: u64 = 180 * 24 * 60 * 60; // 6 months (in seconds)
+    let max_finders_fee_bps: u64 = 1000; // 10%
+    let bid_removal_reward_bps: u64 = 500; // 5%
+
+    let sudo_params = SudoParams {
+        trading_fee_percent: Decimal::percent(trading_fee_bps),
+        ask_expiry: ExpiryRange {
+            min: min_expiry,
+            max: max_expiry,
+        },
+        bid_expiry: ExpiryRange {
+            min: min_expiry,
+            max: max_expiry,
+        },
+        operators: vec![Addr::unchecked("operator")],
+        max_finders_fee_percent: Decimal::percent(max_finders_fee_bps),
+        min_price: Uint128::from(5u128),
+        stale_bid_duration: Duration::Time(100),
+        bid_removal_reward_percent: Decimal::percent(bid_removal_reward_bps),
+        listing_fee: Uint128::from(listing_fee),
+    };
+
+    let return_value = SystemResult::Ok(ContractResult::Ok(
+        to_binary(&ParamsResponse {
+            params: sudo_params.clone(),
+        })
+        .unwrap(),
+    ));
+
+    let mut mock = MockQuerierStruct::new();
+    mock.expect_raw_query()
+        .with(predicate::always())
+        .times(1)
+        .returning(move |_| return_value.clone());
+
+    let querier_wrapper = QuerierWrapper::new(&mock);
+
+    assert_eq!(
+        Ok(sudo_params),
+        load_marketplace_params(&querier_wrapper, &Addr::unchecked("marketplace"))
+    );
+}
+
+#[test]
+fn try_load_collection_royalties() {
+    mock! {
+        QuerierStruct {}
+        impl Querier for QuerierStruct {
+            fn raw_query(&self, bin_request: &[u8]) -> QuerierResult;
+        }
+    }
+
+    let return_value = SystemResult::Ok(ContractResult::Ok(
+        to_binary(&CollectionInfoResponse {
+            creator: "creator".to_string(),
+            description: "description".to_string(),
+            image: "image".to_string(),
+            external_link: None,
+            explicit_content: None,
+            start_trading_time: None,
+            royalty_info: Some(RoyaltyInfoResponse {
+                payment_address: "payment_address".to_string(),
+                share: Decimal::percent(200),
+            }),
+        })
+        .unwrap(),
+    ));
+
+    let mut mock = MockQuerierStruct::new();
+    mock.expect_raw_query()
+        .with(predicate::always())
+        .times(1)
+        .returning(move |_| return_value.clone());
+
+    let querier_wrapper = QuerierWrapper::new(&mock);
+    let deps = mock_dependencies();
+
+    assert_eq!(
+        Ok(Some(RoyaltyInfo {
+            payment_address: Addr::unchecked("payment_address"),
+            share: Decimal::percent(200),
+        })),
+        load_collection_royalties(&querier_wrapper, &deps.api, &Addr::unchecked("collection"))
     );
 }
 
