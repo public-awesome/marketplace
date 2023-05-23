988ad9e936a4c2006ca55dc879c12c9b76a02a7c
Tasio Victoria <tasiovictoria@gmail.com>
2023-05-23 12:53:00 -0400

    Add blank migration message to update testnet contract

--- contracts/reserve-auction/src/lib.rs
+++ contracts/reserve-auction/src/lib.rs
@@ -3,6 +3,7 @@ mod error;
 pub mod execute;
 pub mod helpers;
 pub mod instantiate;
+pub mod migrate;
 pub mod msg;
 pub mod query;
 pub mod state;
---
+++ contracts/reserve-auction/src/migrate.rs
@@ -0,0 +1,12 @@
+use crate::ContractError;
+
+use cosmwasm_std::{DepsMut, Empty, Env};
+use sg_std::Response;
+
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn migrate(_deps: DepsMut, _env: Env, _msg: Empty) -> Result<Response, ContractError> {
+    Ok(Response::new())
+}
