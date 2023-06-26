9cdc1e7ff608c91b23de27718b2601d815a7a871
Tasio Victoria <tasiovictoria@gmail.com>
2023-06-26 12:53:41 -0400

    First pass at creating types workspace package

---
+++ typescript/packages/marketplace-types/README.md
@@ -0,0 +1,11 @@
+# `@stargazezone/marketplace-types`
+
+> TODO: description
+
+## Usage
+
+```
+const marketplaceTypes = require('@stargazezone/marketplace-types');
+
+// TODO: DEMONSTRATE API
+```
---
+++ typescript/packages/marketplace-types/codegen.ts
@@ -0,0 +1,43 @@
+import codegen from "@cosmwasm/ts-codegen"
+
+codegen({
+  contracts: [
+    {
+      name: "Marketplace",
+      dir: "../../../contracts/marketplace/schema"
+    },
+    {
+      name: "ReserveAuction",
+      dir: "../../../contracts/reserve-auction/schema"
+    }
+  ],
+  outPath: "./src/",
+
+  options: {
+    bundle: {
+      bundleFile: "index.ts",
+      scope: "contracts"
+    },
+    types: {
+      enabled: true
+    },
+    client: {
+      enabled: true
+    },
+    reactQuery: {
+      enabled: false,
+      optionalClient: true,
+      version: "v4",
+      mutations: true,
+      queryKeys: true
+    },
+    recoil: {
+      enabled: false
+    },
+    messageComposer: {
+      enabled: true
+    }
+  }
+}).then(() => {
+  console.log("✨ all done!")
+})
