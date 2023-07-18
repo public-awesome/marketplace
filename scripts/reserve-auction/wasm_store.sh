24f409f25db3a8c4af3b0e1e8a1684143db36cfb
Tasio Victoria <tasiovictoria@gmail.com>
2023-07-18 13:32:27 -0400

    Update script

--- scripts/reserve-auction/wasm_store.sh
+++ scripts/reserve-auction/wasm_store.sh
@@ -2,11 +2,12 @@ set -eux
 
 CONTRACT=artifacts/stargaze_reserve_auction.wasm
 
-TITLE="Stargaze Reserve Auction v1.0.0" 
-DESCRIPTION=$(cat scripts/markdown/stargaze_reserve_auction-v1.0.0.md | jq -Rsa | tr -d '"')
+TITLE="Stargaze Live Auction v1.0.0" 
+MARKDOWN="scripts/markdown/stargaze_reserve_auction-v1.0.0.md"
+DESCRIPTION=$(cat "$MARKDOWN" | base64 | tr -d '\n')
 SOURCE="https://github.com/public-awesome/core/releases/tag/stargaze_reserve_auction-v1.0.0"
 BUILDER="cosmwasm/workspace-optimizer:0.12.13"
-HASH="f2674f0df06a37254563b81aefc0dd184874c996a4efaeaaec7199e390128153"
+HASH="f7cdf509a1889e21399c33eee9e68ac328abd4a456142db080d760d15135fe56"
 
 FROM="hot-wallet"
 DEPOSIT="10000000000ustars"
@@ -14,12 +15,12 @@ DEPOSIT="10000000000ustars"
 RUN_AS="stars19mmkdpvem2xvrddt8nukf5kfpjwfslrsu7ugt5"
 ANY_OF_ADDRS="stars19mmkdpvem2xvrddt8nukf5kfpjwfslrsu7ugt5,stars1r5ecq7zn6hwh5e68e79ume8rp9ht7kjz352drk"
 
-CHAIN_ID="elgafar-1"
-NODE="https://rpc.elgafar-1.stargaze-apis.com:443"
+CHAIN_ID="stargaze-1"
+NODE="https://rpc.stargaze-apis.com:443"
 
 starsd tx gov submit-proposal wasm-store "$CONTRACT" \
  --title "$TITLE" \
- --description "$DESCRIPTION" \
+ --description "$(echo "$DESCRIPTION" | base64 --decode)" \
  --code-source-url "$SOURCE" \
  --builder "$BUILDER" \
  --code-hash "$HASH" \
