a813d9c49239b810ae18875681d768759215ee31
Tasio Victoria <tasiovictoria@gmail.com>
2023-07-24 11:40:48 -0400

    Reserve auction instantiate script

--- scripts/reserve-auction/instantiate.sh
+++ scripts/reserve-auction/instantiate.sh
@@ -1,32 +1,34 @@
-RESERVE_AUCTION_ELGAFAR_CODE_ID=2215
+CODE_ID=75
 MSG=$(cat <<EOF
 {
-  "marketplace": "stars18cszlvm6pze0x9sz32qnjq4vtd45xehqs8dq7cwy8yhq35wfnn3qgzs5gu",
-  "min_bid_increment_bps": 50,
-  "min_duration": 10,
-  "max_duration": 31536000,
-  "extend_duration": 1800,
-  "create_auction_fee": {"denom":"ustars","amount":"50"},
-  "max_auctions_to_settle_per_block": 200,
-  "min_reserve_prices": [{"denom":"ustars","amount":"100"}]
+    "fair_burn": "stars1pm9sf7ftn6fmxcsx9cn9cng2r42vfhg7wh58h975cv9tdpcjjrdsgd0lzt",
+    "trading_fee_percent": "0.02",
+    "min_bid_increment_percent": "0.01",
+    "min_duration": 60,
+    "max_duration": 5184000,
+    "extend_duration": 900,
+    "create_auction_fee": {"denom": "ustars", "amount": "5000000"},
+    "max_auctions_to_settle_per_block": 25,
+    "halt_duration_threshold": 300,
+    "halt_buffer_duration": 600,
+    "halt_postpone_duration": 1800,
+    "min_reserve_prices": [{"denom": "ustars", "amount": "1000000"}]
 }
-
 EOF
 )
 
-starsd tx wasm instantiate $RESERVE_AUCTION_ELGAFAR_CODE_ID  "$MSG"  --label "reserve-auction" --admin "stars1vs6ezyqu2mk6x9k5uwvzjx0thsvdhq90u3vvw0" \
-  --from testnet-1 --gas-prices 0.1ustars --gas-adjustment 1.7 --gas auto \
-  -b block -o json | jq .
+FROM="hot-wallet"
+CHAIN_ID="stargaze-1"
+NODE="https://rpc.stargaze-apis.com:443"
 
-# RESERVE_AUCTION_ELGAFAR_CODE_ID=2356
-# MSG=$(cat <<EOF
-# {
-#   "fee_bps": 5000
-# }
-
-# EOF
-# )
-
-# starsd tx wasm instantiate $RESERVE_AUCTION_ELGAFAR_CODE_ID  "$MSG"  --label "sg-fair-burn" --admin "stars1vs6ezyqu2mk6x9k5uwvzjx0thsvdhq90u3vvw0" \
-#   --from testnet-1 --gas-prices 0.1ustars --gas-adjustment 1.7 --gas auto \
-#   -b block -o json | jq .
+starsd tx wasm instantiate $CODE_ID  "$MSG"  \
+  --label "stargaze-live-auction" \
+  --from "$FROM" \
+  --chain-id "$CHAIN_ID" \
+  --node "$NODE" \
+  --gas-prices 1ustars \
+  --gas-adjustment 1.7 \
+  --gas auto \
+  --no-admin \
+  -b block \
+  -o json | jq .
